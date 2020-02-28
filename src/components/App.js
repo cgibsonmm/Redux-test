dcf4c7cbcce60f7b23ad6eca3e479a432d3d2bc3
corey gibson <cgibsonmm@gmail.com>
2020-02-27 20:06:40 -0500

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import PageNotFound from "./PageNotFound";
+import Header from "./common/Header";
+
+export default function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>this app uses React, Redux</p>
+    </div>
+  );
+}
